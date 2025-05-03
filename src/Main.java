526df4319ebbc3223d4ac23c49d42ce357fd75e9
Gabriel Sauer Martín <gabi712004@gmail.com>
2025-05-03 18:41:40 +0200

    Project Functioning

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,10 +12,17 @@ public class Main extends Application {
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception {
         System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "ERROR");
         System.setProperty("org.slf4j.simpleLogger.log.org.mongodb", "off");
 
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/UsuariosView.fxml"));
+        Parent root = loader.load();
+
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Mi aplicación JavaFX");
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
