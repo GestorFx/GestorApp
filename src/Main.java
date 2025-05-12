b769b81ee1c139c45bbc8d40eace4374750c5f04
drakharious <drakharious@gmail.com>
2025-05-12 11:09:32 +0100

    Cambio de nombre en los archivos main.fxml y main.css

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Main.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/main.fxml"));
         Parent root = loader.load();
 
         Scene scene = new Scene(root);
